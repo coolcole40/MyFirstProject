78369107dbfa0cfed725db7fbbb3d2070e212b5f
coolcole40 <124939@bwls.net>
2016-10-13 19:23:32 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,15 +2,13 @@
 
 function setup() {
   createCanvas( windowWidth , windowHeight ); 
- fill(0 , 255 , 255);
   noStroke();
   
 }
 var n;
 function draw() {
- loop( n = random( 50 ,100));
 ellipse( mouseX , mouseY , n , n);
-  
+  fill( 0 , 255 , 255);
  
  
   
