976653fd3a7ff7cab70df65afde7bbd45f6ebcf7
coolcole40 <124939@bwls.net>
2016-10-13 14:53:45 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,4 +4,8 @@ function setup() {
 
 function draw() {
   ellipse( 50 , 50 , 50 , 50 );
+  ellipse( 40 , 40 , 40 , 40 );
+  ellispe( 30 , 30 , 30 , 30 );
+  ellispe( 20 , 20 , 20 , 20 );
+  ellispe( 10 , 10 , 10 , 10 );
 }
