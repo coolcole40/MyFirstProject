3c2c2f74d855f1b69d23ddc95350c78c1edd25fb
coolcole40 <124939@bwls.net>
2016-10-14 13:23:30 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,10 +8,8 @@ function setup() {
 }
 
 function draw() {
-  loop( n = random(10 , 150)  
- r = random(10 , 150)  
-ellipse( mouseX , mouseY , r , n)
-  fill( 0 , r , n)
+  loop( n = random(10 , 150)    
+ellipse( mouseX , mouseY , n , n)
   );
  
  
