cd8d8d8ec9e5cd039f02d1bb6298c01ad944994c
coolcole40 <124939@bwls.net>
2017-02-26 16:15:29 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,19 +1,35 @@
+var ballX=1000;
+var ballY=1000;
+var ballSPD=13;
+var ballSPD2=12;
 
-var n;
 function setup() {
-  createCanvas( windowWidth , windowHeight ); 
-  noStroke();
-  fill ( 0 , 255 , 0);
+  createCanvas( windowWidth , windowHeight );
   
 }
 
 function draw() {
- 
-    n = random(10 , 80);   
-  ellipse( mouseX , mouseY , n , n);
- 
- 
- 
+  background(51);
+  var q = random(50,255);
+  var w = random(50,255);
+  var e = random(50,255);
+  fill(10,10,255);
+  ellipse(ballX,ballY,100,100);
+  if(ballX < 0){
+    ballSPD = 13;
+  }
+  if(ballX > width){
+    ballSPD =-13;
+  }
+  if(ballY > windowHeight){
+    ballSPD2 = -12;
+  }
+  if(ballY < 0){
+    ballSPD2 = 12;
+  }
   
-
+  ballX+=ballSPD;
+  ballY+=ballSPD2;
+  
 }
+
